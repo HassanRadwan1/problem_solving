851ec1f4f6f506b52ffe01afa3c250e298c8fa24
Hassan_Radwan <Hassanradwan161@gmail.com>
2024-02-13 21:14:16 +0200

    This code to print Characters From AAA to ZZZ by all ways

---
+++ wordsfromAAAtoZZZ.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printwordsfromAAAtoZZZ(){
+for(char i='A';i<'Z';i++){
+
+for(char j='A';j<'Z';j++){
+
+for(char k ='A';k<'Z';k++){
+
+
+cout<<i<<j<<k<<endl;
+}
+}}
+
+
+
+}
+
+int main(){
+
+printwordsfromAAAtoZZZ();
+
+
+}
